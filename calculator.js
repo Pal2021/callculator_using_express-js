fdf69047f9fadd5fc5c7d1c461ba97946f80ff99
prashant <prashantpal200013@gmail.com>
2023-06-15 02:03:57 +0530

    first commit

---
+++ calculator.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+app.post("/", function (req, res) {
+  var num1 = Number(req.body.n1);
+  var num2 = Number(req.body.n2);
+  var result = num1 + num2;
+  console.log(req.body);
+  res.send("the result of the calculation is " + result);
+});
+app.listen(3000, function () {
+  console.log("serever running on port 3000");
+});
